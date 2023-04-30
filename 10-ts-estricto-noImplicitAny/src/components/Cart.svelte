9f32e0f7a7b83128dbc4dadab9a629983775b3c7
Ramón Huidobro <hola@ramonh.dev>
2023-04-30 18:06:24 +0200

    Add 10

---
+++ 10-ts-estricto-noImplicitAny/src/components/Cart.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  /*
+   * 👇 Componente para los items en el carrito.
+   */
+
+  // ❗️ A `items`, `amount` y `total` les falta tipo.
+  // Démosles `any[]` a `items` y `any` al resto.
+  export let items;
+  export let amount;
+  export let total;
+</script>
+
+{#if items.length}
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Amount</th>
+      <th>Price</th>
+    </tr>
+    {#each items as row}
+      <tr>
+        <td>{row.name}</td>
+        <td>{row.quantity}</td>
+        <td class="price">${row.priceCents / 100.0}</td>
+      </tr>
+    {/each}
+    <tr>
+      <td><strong>Total</strong></td>
+      <td><strong>{amount}</strong></td>
+      <td class="price"><strong>${total / 100.0}</strong></td>
+    </tr>
+  </table>
+{/if}
+
+<!-- 🛎 Estilos para nuestro componente -->
+
+<style>
+  table {
+    margin-top: 1em;
+    margin-left: auto;
+    margin-right: auto;
+    width: 75%;
+  }
+  th {
+    background-color: var(--primary-color);
+    color: var(--background-color);
+  }
+  td {
+    text-align: left;
+  }
+  td.price {
+    text-align: right;
+  }
+  table,
+  th,
+  td {
+    border: 1px solid var(--primary-color);
+    border-collapse: collapse;
+    padding: 1em;
+  }
+</style>
---
+++ 10-ts-estricto-noImplicitAny/src/components/Menu.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  /*
+   * 👇 Componente para los dulces disponibles
+   */
+
+  import { availableCandies } from "../types/Candy";
+  // ❗️ A `addCartItem` le falta tipo. Démosles `any`.
+  export let addCartItem: any;
+</script>
+
+<div class="flex">
+  <!-- 🛎 Realizar un bucle sobre los dulces disponibles y 
+  renderizar botones para ellos -->
+  {#each availableCandies as candy}
+    <button on:click={(_e) => addCartItem(candy)}>
+      {candy.name()}<br />${candy.priceCents() / 100.0}
+    </button>
+  {/each}
+</div>
+
+<!-- 🛎 Estilos para nuestro componente -->
+
+<style>
+  .flex {
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 75%;
+    justify-content: space-between;
+    justify-items: center;
+    gap: 6px;
+  }
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: var(--background-color);
+    background-color: var(--primary-color);
+    border-radius: 2em;
+    border: 2px solid rgba(255, 62, 0, 0);
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+  }
+  button:focus {
+    border: 2px solid #ff3e00;
+  }
+  button:active {
+    background-color: rgba(255, 62, 0, 0.2);
+  }
+</style>
