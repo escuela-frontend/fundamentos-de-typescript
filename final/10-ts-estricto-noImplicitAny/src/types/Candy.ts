542bf49dab99938cda038c2d41a22ab453633cd8
Ramón Huidobro <hola@ramonh.dev>
2023-04-30 18:05:58 +0200

    Add 10

---
+++ final/10-ts-estricto-noImplicitAny/src/types/Candy.ts
@@ -0,0 +1,44 @@
+/*
+ * 👇 Clase abstracta y subclases para los dulces.
+ * Tienen un nombre y precio.
+ */
+
+export abstract class Candy {
+  // ❗️ A `priceCents` y `name` les falta tipo. Démosles `any`.
+  abstract priceCents(): any;
+  abstract name(): any;
+}
+
+class Chocolate extends Candy {
+  priceCents() {
+    return 99;
+  }
+  name() {
+    return 'Chocolate';
+  }
+}
+
+class Caramel extends Candy {
+  priceCents() {
+    return 199;
+  }
+  name() {
+    return 'Caramelo';
+  }
+}
+
+class Marzipan extends Candy {
+  priceCents() {
+    return 499;
+  }
+  name() {
+    return 'Mazapán';
+  }
+}
+
+// 🛎 Todos los dulces
+export const availableCandies = [
+  new Chocolate(),
+  new Caramel(),
+  new Marzipan(),
+];
